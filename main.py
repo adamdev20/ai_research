1acf49d517320b49d57c725e8407c386f3ba1ab9
Adam Raja Garam <114385858+adamdev20@users.noreply.github.com>
2026-06-04 13:51:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+import os
 
+webhook = os.environ["DISCORD_WEBHOOK"]
+
+requests.post(
+    webhook,
+    json={
+        "content": "🚀 Agent berhasil berjalan!"
+    }
+)
+
+print("sent")
